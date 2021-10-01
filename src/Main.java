84f12c0fc7cab92ff193c9f5312fcf49cd3aa611
skqhfla <22000216@handong.edu>
2021-10-01 14:10:19 +0900

    5주차 final 제

--- src/Main.java
+++ src/Main.java
@@ -22,10 +22,11 @@ public class Main {
 				System.out.println(id + " " + name);
 			}
 			stat1.close();
+			
 			//데이터 추가
 			System.out.println("\n*** 새 데이터 추가 ***");
 			Statement stat2 = con.createStatement();
-			String sql2 = "insert into g_artists (name, a_type, a_year, debut, regdate)" + " values ('방탄소년단', '남', '2010년대', '2013년',datetime('now', 'localtime'));";
+			String sql2 = "insert into g_artists (name, a_type, a_year, debut, regdate)" + " values ('아이유', '여자', '2000년대, 2010년대, 2020년', '2008년',datetime('now', 'localtime'));";
 			int cnt = stat2.executeUpdate(sql2);
 			if(cnt > 0)
 					System.out.println("새로운 데이터가 추가되었습니다!");
@@ -36,24 +37,24 @@ public class Main {
 			//데이터 수정
 			System.out.println("\n*** 데이터 수정 ***");
 			Statement stat3 = con.createStatement();
-			String sql3 = "update g_artists set a_year = '2000년대, 2010년대, 2020년대" + "where id=1 ;";
+			String sql3 = "update g_artists set a_year = '2000년대, 2010년대, 2020년대'" + "where id=1 ;";
 			int cnt3 = stat3.executeUpdate(sql3);
 			if(cnt3 > 0)
 				System.out.println("데이터가 수정되었습니다!");
 			else
 				System.out.println("[Error] 데이터 수정 오류!");
-		stat3.close();
+			stat3.close();
 		
-		//데이터 삭제
-		System.out.println("\n*** 데이터 삭제 ***");
-		Statement stat4 = con.createStatement();
-		String sql4 = "delete from g_artists where id=6 ;";
-		int cnt4 = stat4.executeUpdate(sql4);
-		if(cnt4 > 0)
-			System.out.println("데이터가 삭제되었습니다!");
-		else
-			System.out.println("[Error] 데이터 삭제 오류!");
-		stat4.close();
+			//데이터 삭제
+			System.out.println("\n*** 데이터 삭제 ***");
+			Statement stat4 = con.createStatement();
+			String sql4 = "delete from g_artists where id=6 ;";
+			int cnt4 = stat4.executeUpdate(sql4);
+			if(cnt4 > 0)
+				System.out.println("데이터가 삭제되었습니다!");
+			else
+				System.out.println("[Error] 데이터 삭제 오류!");
+			stat4.close();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
